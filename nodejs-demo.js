72da82c582a811f6446ceec574d72f4c5313b04c
屈冠文 <39824558+quguanwen@users.noreply.github.com>
2020-12-17 12:15:08 +0800

    Create nodejs-demo.js

---
+++ nodejs-demo.js
@@ -0,0 +1,8 @@
+console.log("Hello World");
+var http = require("http");
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(8888);
